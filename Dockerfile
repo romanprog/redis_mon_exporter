c111da0e8d5495c688537bea250aa6da16732dc0
Roman Progonnyj <roman.progonnyj@gmail.com>
2018-10-03 14:13:16 +0300

    multi-stage build

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM golang:alpine
+
+ADD main.go /tmp/
+
+RUN  apk add --no-cache --update git \
+  && go get github.com/garyburd/redigo/redis github.com/gorilla/mux \
+  && CGO_ENABLED=0 GOOS=linux go build -o /usr/bin/redis_mon_exporter /tmp/main.go
+
+FROM alpine
+
+RUN apk --no-cache add ca-certificates
+WORKDIR /usr/bin/
+COPY --from=0 /go/src/github.com/alexellis/href-counter/app .
+
+ENTRYPOINT ["/usr/bin/redis_mon_exporter"]
--- main.go
+++ main.go
@@ -13,16 +13,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var redisServers = flag.String("redis_servers", getEnv("REDIS_SERVERS", "localhost:6379"), "Address list in format 'host:port,host2:port'")
+var redisServers = flag.String("redis.servers", getEnv("REDIS_SERVERS", "localhost:6379"), "Address list in format 'host:port,host2:port'")
+var listenPort = flag.String("listen.port", getEnv("LISTEN_PORT", "8080"), "Listen on port. Default 8080")
 
-// main function to boot up everything
 func main() {
 	flag.Parse()
-	fmt.Println(*redisServers)
-
 	router := mux.NewRouter()
 	router.HandleFunc("/metrics", DoTests).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	listenUrl := fmt.Sprintf("0.0.0.0:%s", *listenPort)
+	log.Printf("Runing listener on %s", listenUrl)
+	log.Fatal(http.ListenAndServe(listenUrl, router))
 }
 
 func getEnv(key string, defaultVal string) string {
@@ -33,24 +33,25 @@ func getEnv(key string, defaultVal string) string {
 }
 
 func testRedis(server string) int {
-	//srvArr := strings.Split(server, ":")
-	fmt.Println("Test")
 	c, err := redis.Dial("tcp", server)
 	if err != nil {
-		log.Printf("Could not connect: %v\n", err)
+		log.Printf("Could not connect: endpoint: %s. Error: %v\n", server, err)
 		return 1
 	}
 	defer c.Close()
 	tm, _ := time.ParseDuration("1s")
 	ret, err := redis.DoWithTimeout(c, tm, "SET", "gomonitor", "1")
 	if (err != nil) || (ret != "OK") {
+		log.Printf("Could not write: endpoint: %s. Error: %v\n", server, err)
 		return 1
 	}
+	log.Printf("Check OK: endpoint: %s. Result: %s", server, ret)
 	return 0
 }
 
 func DoTests(w http.ResponseWriter, r *http.Request) {
-	lineTemplate := "redis_mon_write_check{instance=\"%s\"} %b"
+	lineTemplate := "redis_mon_write_check{instance=\"%s\"} %b\n"
+	log.Printf("Request from host: %s", r.Host)
 	for _, serverUrl := range strings.Split(*redisServers, ",") {
 		res := testRedis(serverUrl)
 		metricLine := fmt.Sprintf(lineTemplate, serverUrl, res)
